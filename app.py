5c25fd0733ece7af7706018b14f8ce9416d5038b
hyomin001 <155884845+hyomin001@users.noreply.github.com>
2026-04-09 08:59:57 +0900

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ import os
 import time
 import tempfile
 import shutil
-from datetime import datetime, timedelta, timezone
+os.environ['TZ'] = 'Asia/Seoul'
+try: time.tzset()
+except AttributeError: pass
+
+from datetime import datetime
 
 # 그 바로 아랫줄에 한국 시간(KST) 세팅을 추가해 줍니다.
 KST = timezone(timedelta(hours=9))
