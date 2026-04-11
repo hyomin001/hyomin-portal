88d4589d57a5413f27d4039f4ca7f90ec773556d
hyomin001 <155884845+hyomin001@users.noreply.github.com>
2026-04-11 14:31:35 +0900

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import tempfile
 import shutil
 import uuid
 from datetime import datetime, timedelta, timezone
-ADMIN_PW = os.environ.get("ADMIN_PW", "****")
+ADMIN_PW = "5891"
 
 # ==============================
 # 🕒 서버 시간 강제 세팅 (KST)
