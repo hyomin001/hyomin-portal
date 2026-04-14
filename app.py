c430f4168e90b5b9ff93c5af62c21d0555003808
hyomin001 <155884845+hyomin001@users.noreply.github.com>
2026-04-14 16:16:04 +0900

    app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def hash_pw(pw: str) -> str:
     return hashlib.sha256(pw.encode('utf-8')).hexdigest()
 
 ADMIN_HASH = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4" 
+ADMIN_PW = "***"
 
 
 
