977fdba1ddaa407487e19806f56b590652a8e52a
hyomin001 <155884845+hyomin001@users.noreply.github.com>
2026-05-06 18:56:34 +0900

    project_g.py

--- pages/project_g.py
+++ pages/project_g.py
@@ -1201,7 +1201,7 @@ def render():
 
     listener_html = """
     <script>
-    window.addEventListener('message', function(e) {
+    window.parent.addEventListener('message', function(e) {
       if (e.data && e.data.type === 'zombie_result') {
         const url = new URL(window.parent.location.href);
         url.searchParams.set('zombie_wave',  e.data.wave);
